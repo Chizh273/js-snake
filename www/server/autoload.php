fa8aba0448107dfa6e2bcf6ccba670317bbad870
chizh273 <chizh273@gmail.com>
2016-05-09 10:45:18 +0300

    autoload

--- www/server/autoload.php
+++ www/server/autoload.php
@@ -1,2 +1,10 @@
 <?php
 
+    function __autoload( $class )
+    {
+        $classPath = explode( '\\', $class );
+        $classPath[0] = __DIR__;
+        $path = implode( DIRECTORY_SEPARATOR, $classPath ) . ".php";
+        if ( file_exists( $path ) )
+            require $path;
+    }
