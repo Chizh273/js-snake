cf05f361818ec02aa1c33c2617d107cac5c83800
chizh273 <chizh273@gmail.com>
2016-05-12 20:15:27 +0300

    added method for fetch colon Name

--- www/server/ajax.php
+++ www/server/ajax.php
@@ -2,13 +2,14 @@
 
     require __DIR__ . "/autoload.php";
 
-    if ( isset( $_POST ) && !empty( $_POST ) ) {
+    if ( $_POST['action'] === "name" ) {
+        echo json_encode( \App\model\ModelUser::getAllColonsName() );
+    } else if ( isset( $_POST ) && !empty( $_POST ) ) {
         $model = new \App\model\ModelUser();
         foreach ( $_POST as $key => $value ) {
             $model->$key = $value;
         }
         $model->insert();
     } else {
-//        var_dump( \App\model\ModelUser::findAll() );
         echo json_encode( \App\model\ModelUser::findAll() );
     }
--- www/server/model/ModelUser.php
+++ www/server/model/ModelUser.php
@@ -6,4 +6,16 @@
     class ModelUser extends \App\classes\AbstractModel
     {
         static public $table = "user";
+
+        static public function getAllColonsName()
+        {
+            $class = get_called_class();
+            $sql = "SELECT `name` FROM `" . static::$table . "` WHERE 1";
+
+            $db = new \App\classes\DB();
+            $db->setClassName( $class );
+            $res = $db->query( $sql );
+
+            return $res;
+        }
     }
